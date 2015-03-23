7265df7879703c504fa8a7388bd9e4ba760ffff8
Jesse Shawl <jesse@jshawl.com>
2015-03-23 15:30:22 -0400

    add in class work

--- w02/d01_object_oriented_programming/02_implementing_objects/orange_tree.rb
+++ w02/d01_object_oriented_programming/02_implementing_objects/orange_tree.rb
@@ -1,20 +1,51 @@
+$global_var = "evil :("
+
 class OrangeTree
-  def initialize height = 0
+  attr_accessor :height, :age
+  @@total_orange_trees = 0
+  def initialize height = 0, age = 0
+    puts $global_var
+    @@total_orange_trees += 1
+    #puts "#{@@total_orange_trees} total oranges so far."
+    @age = age
     @height = height 
   end
-  def height
-    @height
-  end
   def one_year_passes
+    age = "#{@age} years old"
+    puts "age is #{age}"
     @height = @height + 1
+  end
+  def self.total_trees
+    @@total_orange_trees
   end
 end
 
-tree = OrangeTree.new(36)
-p tree.one_year_passes
-tree = OrangeTree.new
-p tree.one_year_passes
-p tree.one_year_passes
-p tree.one_year_passes
-p tree.one_year_passes
-p tree.one_year_passes
+tree = OrangeTree.new(36, 2)
+tree = OrangeTree.new(36, 2)
+tree = OrangeTree.new(36, 2)
+tree = OrangeTree.new(36, 2)
+tree = OrangeTree.new(36, 2)
+
+puts OrangeTree.total_trees
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
