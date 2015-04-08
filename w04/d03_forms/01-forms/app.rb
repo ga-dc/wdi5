2b1cc5d14e9c2dc02bbb8cb86ddcfb1366b75ad2
Jesse Shawl <jesse@jshawl.com>
2015-04-08 14:16:57 -0400

    add in class solution

--- w04/d03_forms/01-forms/app.rb
+++ w04/d03_forms/01-forms/app.rb
@@ -4,5 +4,15 @@ require 'sinatra/reloader'
 $students = ['Jamie','Jameel','Janet']
 get '/' do
   @students = $students
+  if params["query"]
+    @students = @students.select{ |student|
+      student.match( params["query"] ) 
+    }
+  end
   erb :index
+end
+
+post '/students' do
+  $students << params["student_name"]
+  redirect '/'
 end
