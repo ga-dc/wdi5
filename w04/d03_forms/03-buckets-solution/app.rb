17492c9ab84712aba9a36a3836be707384e41d3e
Jesse Shawl <jesse@jshawl.com>
2015-04-08 19:21:52 -0400

    add index and show views

--- w04/d03_forms/03-buckets-solution/app.rb
+++ w04/d03_forms/03-buckets-solution/app.rb
@@ -1,7 +1,17 @@
 require 'pry'
 require 'active_record'
+require 'sinatra'
+require 'sinatra/reloader'
 require_relative 'models/item'
 require_relative 'models/comment'
 require_relative 'db/connection'
 
-# Your routes here...
+get '/' do
+  @items = Item.all
+  erb :"items/index"
+end
+
+get '/items/:id' do
+  @item = Item.find( params[:id] )
+  erb :"items/show"
+end
