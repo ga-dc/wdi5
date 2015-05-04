57cce215cc56201ea3481d558b655c4be74fd746
Jesse Shawl <jesse@jshawl.com>
2015-05-04 17:00:50 -0400

    refactor board

--- w08/d01_object_oriented_javascript/exercises/trello/starter/js/views/board.js
+++ w08/d01_object_oriented_javascript/exercises/trello/starter/js/views/board.js
@@ -9,49 +9,39 @@ var BoardView = function( selector, model ){
 
 BoardView.prototype = {
   bindUI: function(){
-    this.els.newBoard.addEventListener("click", this.newBoard.bind(this) ) 
+    this.els.newBoard.addEventListener("click", function( event ){
+      event.preventDefault()
+      this.newBoard()
+    }.bind(this)) 
     $("body").on("keyup", ".js-board-create", this.createBoard.bind(this) )
   },
-  newBoard: function( event ){
-    event.preventDefault()
+  newBoard: function( titleText ){
     var board = document.createElement("div") 
     board.className = "board"
-    var input = document.createElement("input")
-    input.className = "js-board-create"
     var title = document.createElement("h2")
     title.className = "board-title"
-    title.appendChild( input )
+    if( titleText ){
+      title.innerHTML = titleText 
+    } else {
+      var input = document.createElement("input")
+      input.className = "js-board-create"
+      title.appendChild( input )
+    }
     board.appendChild( title )
-    this.els.boards.appendChild( board )
+    $(this.els.boards).prepend( board )
   },
   createBoard: function( event ){
-    console.log( event )
     if( event.keyCode == 13 ){
       var title = event.target.value
       var board = new Board( title )
-      console.log( board )
       this.model.all.push( board )
       this.render()
     }
   },
-  render: function( kind ){
+  render: function(){
     this.els.boards.innerHTML = ""
-    for( var i = 0; i < this.model.all.length; i++ ){
-      var board = document.createElement("div") 
-      board.className = "board"
-      var title = document.createElement("h2")
-      title.className = "board-title"
-      if( this.model.all[i].title ){
-	var titleText = this.model.all[i].title
-	title.innerHTML = titleText
-      } else {
-	var input = document.createElement("input")
-	input.className = "js-board-create"
-	title.appendChild( input )
-	input.focus()
-      }
-      board.appendChild( title )
-      this.els.boards.appendChild( board )
+    for( var i = 0; i  < this.model.all.length; i++ ){
+      this.newBoard( this.model.all[i].title )
     }
   }
 }
