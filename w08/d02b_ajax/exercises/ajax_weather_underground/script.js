79841011aef07d1a739573bfd5548b3a7db69bf6
Andy <andrew.sunglae.kim@gmail.com>
2015-05-06 12:20:54 -0400

    add ajax request

--- w08/d02b_ajax/exercises/ajax_weather_underground/script.js
+++ w08/d02b_ajax/exercises/ajax_weather_underground/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  alert("test")
+  var weatherUndergroundUrl = "http://api.wunderground.com/api/b0900ea853660a3f/conditions/q/CA/San_Francisco.json"
+  $.ajax({
+    url: weatherUndergroundUrl,
+    type: "GET",
+    dataType: "json"
+  }).done(function(response){
+    console.log(response.current_observation.temp_f)
+  }).fail(function(){
+    console.log("ajax request not successful")
+  }).always(function(){
+    console.log("this always happens!")
+  })
 })
