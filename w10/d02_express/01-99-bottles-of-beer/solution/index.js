2ad2b90368fadfbb926bf2e1713960d2a71100a4
Jesse Shawl <jesse@jshawl.com>
2015-05-18 18:59:44 -0400

    add views to 99 bottles

--- w10/d02_express/01-99-bottles-of-beer/solution/index.js
+++ w10/d02_express/01-99-bottles-of-beer/solution/index.js
@@ -1,18 +1,14 @@
 var express = require("express")
 var app = express()
+var hbs = require("hbs")
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+hbs.registerPartials(__dirname+"/views/partials")
 
-app.get("/", function( req, res ){
-  res.send("99 Bottles of beer on the wall. <a href='/98'>Take One Down, Pass it Around</a>")
-})
-
-app.get("/:numberOfBottles", function( req, res ){
-  var num = req.params.numberOfBottles
-  var next = num - 1
-  if( num != 0 ){
-    res.send(num+" Bottles of beer on the wall. <a href='"+ next +"'>Take One Down, Pass it Around</a>")
-  } else {
-    res.send(num+" Bottles of beer on the wall. <a href='/99'>Start Over</a>")
-  }
+app.get("/:numberOfBottles?", function( req, res ){
+  var bottles = req.params.numberOfBottles || 99
+  var next = bottles - 1
+  res.render("index", {bottles: bottles, next: next})
 })
 
 app.listen(3000, function(){
