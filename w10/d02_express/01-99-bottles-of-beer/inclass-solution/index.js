dc1e6d844ce8e221d25bd7a8b7e692486bdd1424
Jesse Shawl <jesse@jshawl.com>
2015-05-19 14:46:52 -0400

    add in class solution

--- w10/d02_express/01-99-bottles-of-beer/inclass-solution/index.js
+++ w10/d02_express/01-99-bottles-of-beer/inclass-solution/index.js
@@ -1,18 +1,10 @@
 var express = require("express")
 var app = express()
+var routes = require("./routes")
 
-app.get("/", function( req, res ){
-  res.send("99 Bottles of Beer on the wall. <a href='/98'>Take one down</a>")
-})
-
-app.get("/:numBottles", function( req, res ){
-  var next = req.params.numBottles - 1
-  if( next == -1 ){
-    res.send("<a href='/'>Start Over</a>")
-  } else {
-    res.send(req.params.numBottles+" Bottles of Beer on the wall. <a href='/"+next+"'>Take one down</a>")
-  }
-})
+app.set("view engine", "hbs")
+app.use(express.static(__dirname + "/public"))
+app.get("/:numBottles?", routes.index )
 
 app.listen(3000, function(){
   console.log("listening on 3000")
