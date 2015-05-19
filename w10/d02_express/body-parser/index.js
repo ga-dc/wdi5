9ea8021cd99ec4a66aa3b1559e601976005abde5
Jesse Shawl <jesse@jshawl.com>
2015-05-19 15:59:13 -0400

    add in class body parser solution

--- w10/d02_express/body-parser/index.js
+++ w10/d02_express/body-parser/index.js
@@ -1,6 +1,10 @@
 var express = require("express")
 var app = express()
-var hbs = require("hbs")
+var bodyParser = require("body-parser")
+
+app.use( bodyParser.json() ) // handles json POST resquests
+app.use( bodyParser.urlencoded({ extended: true }) ) // handles form submissions
+
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
@@ -8,6 +12,24 @@ app.get("/", function( req, res ){
   res.render("index")
 })
 
+app.post("/", function( req, res ){
+  res.render("index",{
+    name: req.body.name
+  })
+})
+
 app.listen(3000, function(){
   console.log("app listening on port 3000")
 })
+
+
+
+
+
+
+
+
+
+
+
+	
