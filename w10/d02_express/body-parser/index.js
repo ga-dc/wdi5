6011b67c7a6f98653b7eae7ba3914dc527a1a359
Jesse Shawl <jesse@jshawl.com>
2015-05-19 15:00:13 -0400

    remove solution from body parser example

--- w10/d02_express/body-parser/index.js
+++ w10/d02_express/body-parser/index.js
@@ -1,19 +1,11 @@
 var express = require("express")
 var app = express()
 var hbs = require("hbs")
-var bodyParser = require("body-parser")
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-hbs.registerPartials(__dirname+"/views/partials")
 
 app.get("/", function( req, res ){
   res.render("index")
-})
-
-app.post("/", function( req, res ){
-  res.render("index", {name: req.body.name})
 })
 
 app.listen(3000, function(){
